73fd404096c690ba1cdb6c6aa62ca8b1edbd62e5
Daniel <nguyenhoangviety8866@gmail.com>
2024-03-06 09:01:10 +0700

    binary search tree?

--- binary search/e108. Convert Sorted Array to Binary Search Tree.js
+++ binary search/e108. Convert Sorted Array to Binary Search Tree.js
@@ -1,1 +1,24 @@
 // https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/description/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function (nums) {
+    if (nums.length == 0) return null
+    let l = 0
+    let r = nums.length
+    let mid = l + Math.floor((r - l) / 2)
+    let root = new TreeNode(nums[mid])
+    root.left = sortedArrayToBST(nums.slice(0, mid))
+    root.right = sortedArrayToBST(nums.slice(mid + 1))
+    return root
+};
--- binary search/e700. Search in a Binary Search Tree.js
+++ binary search/e700. Search in a Binary Search Tree.js
@@ -1,1 +1,32 @@
 // https://leetcode.com/problems/search-in-a-binary-search-tree/description/
+
+
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function (root, val) {
+    if (!root) return null
+    if (root.val == val) return root
+    if (root.val > val) return searchBST(root.left, val)
+    else return searchBST(root.right, val)
+};
+
+let a = new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(7))
+let b = searchBST(a, 8)
+''
